1cb52a617b2504398abba27dc6cd310c014a228f
shuxinhuan <vlanxin11@gmail.com>
2023-06-29 10:49:21 +0100

    deal with nodes

--- src/components/templates/genSpec.ts
+++ src/components/templates/genSpec.ts
@@ -132,48 +132,117 @@ export const genSpecFromLinkAndNodeTable = (config: NetworkConfig, visType: stri
   const timeColumn = config.linkTableConfig?.time
   const timeFormat = config.linkTableConfig?.timeFormat
 
-  const parse = timeColumn ? { 'parse': { [timeColumn]: `date:'${timeFormat}'` } } : null
+  const defaultNodeType = "_default_node_type"
 
+  const parse = timeColumn ? { 'parse': { [timeColumn]: `date:'${timeFormat}'` } } : null
   const linkTableImportSpec = {
     name: "links",
     localStorage: "UPLOADED_FILE_" + linkFileName,
     format: {
       type: "csv", // TODO: add more types
       ...parse
-    }
+    },
+    transform: [
+      {
+        "type": "calculate",
+        "as": "source",
+        "calculate": `datum.${sourceLabel}`
+      },
+      {
+        "type": "calculate",
+        "as": "target",
+        "calculate": `datum.${targetLabel}`
+      }
+    ] // by default. no need to map node field to data field. just use source
   }
 
-  let dataSpec = [linkTableImportSpec]
-  if (config.extraNodeConfig?.hasExtraNode) {
-    dataSpec = [
-      linkTableImportSpec,
+  if (visType === 'timearcs') {
+    linkTableImportSpec.transform = [
       {
-        "name": "nodes",
-        "localStorage": "UPLOADED_FILE_" + config.extraNodeConfig.file,
-        "format": { type: "csv" }
+        "type": "calculate",
+        "as": "source",
+        "calculate": `datum.${sourceLabel} + ' (' + datum.${timeColumn} + ')'`
+      },
+      {
+        "type": "calculate",
+        "as": "target",
+        "calculate": `datum.${targetLabel} + ' (' + datum.${timeColumn} + ')'`
       }
     ]
   }
 
-  // is "id" correct default for case when only link-table used?
-  const idField = config.extraNodeConfig?.hasExtraNode ? config.extraNodeConfig.nodeID : "id" 
+  let dataSpec: any[] = [linkTableImportSpec]
+  dataSpec = [
+    linkTableImportSpec,
+    {
+      "name": "nodes",
+      "localStorage": "UPLOADED_FILE_" + config.extraNodeConfig?.file,
+      "format": { type: "csv" }
+    }
+  ]
 
+  // is "id" correct default for case when only link-table used?
+  const idField = config.extraNodeConfig?.nodeID
+
+  /**
+   * @description
+   * @param {string} type is this node 'source' or 'target'?
+   * @return {*} 
+   */
+  const getNodeRawData = (type: string) => {
+    const f = (type === 'source') ? sourceLabel : targetLabel
+    if (visType === 'timearcs') {
+      return [
+        { "field": f, "as": "name" },
+        { "field": timeColumn, "as": "date" }
+      ]
+    }
+    else { // 'nodelink', 'matrix'
+      return [
+        { "field": f, "as": "name" },
+      ]
+    }
+  }
 
   const networkSpec = {
     name: "network",
-    links: "links",
-    nodes: config.extraNodeConfig?.hasExtraNode ? "nodes" : undefined,
-    directed: config.linkTableConfig?.directed,
+    "parts": [
+      {
+        "data": "links",
+        "yieldsNodes": [
+          {
+            "id_field": "source",
+            "type": defaultNodeType,
+            "data": getNodeRawData('source')
+          },
+          {
+            "id_field": "target",
+            "type": defaultNodeType,
+            "data": getNodeRawData('target')
+          }
+        ],
+        "yieldsLinks": [
+          {
+            "source_id": { "field": "source" },
+            "source_node_type": defaultNodeType,
+            "source_id_field": "id",
 
-    source_node: [sourceLabel, idField],
-    target_node: [targetLabel, idField],
+            "target_id": { "field": "target" },
+            "target_node_type": defaultNodeType,
+            "target_id_field": "id",
 
-    transform: [
-      { "type": "metric", "metric": "degree" },
-    ] as any[],
+            "addReverseLinks": visType === 'matrix' ? true : false,
+            "data": ["*"]
+          }
+        ]
+      }
+    ],
+    "transform": [
+      { "type": "metric", "metric": "degree" }
+    ] as any[]
   }
 
-  // TODO: deal with geo config
+  // TODO: deal with geo config, linkID
   if (config.linkTableConfig?.linkType)
     networkSpec.transform.push({ "type": "calculate", "as": "linkType", "calculate": `datum.data.${config.linkTableConfig?.linkType}`, "for": "links" })
   if (config.linkTableConfig?.linkWeight)
--- src/components/templates/templates.ts
+++ src/components/templates/templates.ts
@@ -31,18 +31,24 @@ const templates: Template[] = [
     image: "adjacencyList.png",
     template: "adjacencyList.json"
   },
-  // {
-  //   key: "arcDiagram",
-  //   label: "Arc diagram",
-  //   image: "arcDiagram.png",
-  //   template: "arcDiagram.json"
-  // },
   {
-    key: "map",
-    label: "Map",
-    image: "map.png",
-    template: "map.json"
+    key: "arcDiagram",
+    label: "Arc diagram",
+    image: "arcDiagram.png",
+    template: "arcDiagram.json"
   },
+  {
+    key: "jigsaw",
+    label: "Jigsaw",
+    image: "jigsaw.png",
+    template: "jigsaw.json"
+  },
+  // {
+  //   key: "map",
+  //   label: "Map",
+  //   image: "map.png",
+  //   template: "map.json"
+  // },
 ]
 
 export default templates
