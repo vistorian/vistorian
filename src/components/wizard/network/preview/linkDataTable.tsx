fe6fcce9f52d121f4d928064cd6725419b389f8b
shuxinhuan <vlanxin11@gmail.com>
2023-07-11 10:56:42 +0100

    sync

--- src/components/wizard/network/preview/linkDataTable.tsx
+++ src/components/wizard/network/preview/linkDataTable.tsx
@@ -1,7 +1,20 @@
-import { Checkbox, Table, Typography } from 'antd'
+import { Checkbox, Select, Table, Typography } from 'antd'
 import { NetworkConfig } from '../../../../../typings';
 
 const { Title } = Typography
+
+const opts = [
+  { value: '', label: '-'},
+  { value: 'linkId', label: 'Link id' },
+  { value: 'sourceNodeLabel', label: 'Source node label' },
+  { value: 'targetNodeLabel', label: 'Target node label' },
+  { value: 'locationOfSourceNode', label: 'Location of source node' },
+  { value: 'locationOfTargetNode', label: 'Location of target node' },
+  { value: 'linkWeight', label: 'Link weight' },
+  { value: 'linkType', label: 'Link type' },
+  { value: 'time', label: 'Time' },
+]
+const fullCol = opts.map(o => o.value).filter(i => i !== '')
 
 interface IDataTableProps {
   network: NetworkConfig
@@ -27,16 +40,58 @@ function LinkDataTable(props: IDataTableProps) {
   const directed = network.linkTableConfig?.directed
   const timeFormat = network.linkTableConfig?.timeFormat
 
+
+  /**
+   * @description map the data column to network config
+   * @param {string} dataColumn column name in the data table
+   * @return {string} network config, e.g., sourceNodeLable, linkType 
+   */
+  const getDefault = (dataColumn: string) => {
+    if (network.linkTableConfig) {
+      const entries = Object.entries(network.linkTableConfig)
+      for (let i = 0; i < entries.length; i++){
+        const entry = entries[i]
+        if (fullCol.findIndex(i => i === entry[0]) > -1 && dataColumn === entry[1]){
+          return entry[0]
+        }
+      }
+    }
+    return ''
+  }
+
+  const handleChange = (value: string, dataColumn: string) => {
+    if (value !== '') {
+      const tmp = JSON.parse(JSON.stringify(network))
+      // @ts-ignore
+      tmp.linkTableConfig[value] = dataColumn
+    }
+  }
+
   return (
     <>
       <div style={{ display: 'flex'}}>
         <Checkbox />
+      </div>
+      <div style={{ display: 'flex'}}>
+        {columns.map(column => {
+          const netColumn = getDefault(column.dataIndex)
+          return (
+            <Select
+              defaultValue={netColumn}
+              style={{ width: `calc(100%/${Object.keys(jsonData[0]).length} + 8px)`, margin: 4 }}
+              onChange={(value) => handleChange(value, column.dataIndex)}
+              options={opts}
+              disabled={!edit}
+            />
+          )
+        })}
       </div>
       <Table
         columns={columns}
         dataSource={data}
         showHeader={true}
         rowKey={(record) => record._rowKey}
+        pagination={{ defaultPageSize: 5 }}
         size='small'
       />
     </>
