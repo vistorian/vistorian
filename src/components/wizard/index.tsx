60fbcc7ed6be0111ddecc6272f82b36d065f80f5
benjbach <benj.bach@gmail.com>
2024-05-30 06:48:03 +0200

    change button to 'New Network'

--- src/components/wizard/index.tsx
+++ src/components/wizard/index.tsx
@@ -313,7 +313,7 @@ function Wizard() {
                 setNetSource(undefined)
               }}
             >
-              Create New Visualization
+              New Network
             </Button>
           </div>
           
